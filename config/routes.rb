ec768608e4a9d7636a1933b2aa7ca8d847cf960b
matheusperrut <matheus_perrut@id.uff.br>
2018-03-28 17:57:37 +0000

    Alter Table Users (14:57)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users
   resources :sessions, only:  [:create, :destroy]
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327013819) do
+ActiveRecord::Schema.define(version: 20180328175641) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.integer "cpf"
+    t.string "cpf"
+    t.date "birth"
+    t.integer "tel"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "authentication_token", limit: 30
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -27,7 +30,6 @@ ActiveRecord::Schema.define(version: 20180327013819) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "authentication_token", limit: 30
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
