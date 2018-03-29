92be881b66783b862beb2a773bc66c0c6ab2a5be
Mariano <copesmariano@gmail.com>
2018-03-29 15:38:24 -0300

    api-login ready

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user = User.where(email: params[:email]).first
 
     if user && user.valid_password?(params[:password])
-      render json: user.as_json(only: [:email, :authentication_token]), status: :created
+      render json: user.as_json(only: [:email, :name, :authentication_token]), status: :created
 
 
     else
