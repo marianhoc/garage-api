1c53fe48bdf248b22b27070610361f354e5bba6b
Matheus Perrut <matheus.perrut2302@gmail.com>
2018-05-04 00:52:00 +0000

    Seed de Parceiros 03/05 21:51

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,11 +26,12 @@ Estacionamento.destroy_all
               razao_social: Faker::Name.name,
               cnpj: rand(11111111 .. 99999999)
               )
-
-  Parceiro.destroy_all
+  end
+  
+Parceiro.destroy_all
   100.times do |index|
     Parceiro.create(nome: Faker::Name.name,
                 cnpj: rand(11111111 .. 99999999),
                 telefone: rand(11111111 .. 99999999)
                 )
-end
+  end
