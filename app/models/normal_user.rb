23900cf2c86e39ae01eef120aaf774414027f92b
Matheus Perrut <matheus.perrut2302@gmail.com>
2018-05-18 14:39:44 -0300

    Adicionando to_s porque o ionic passa números como string

--- app/models/normal_user.rb
+++ app/models/normal_user.rb
@@ -3,12 +3,12 @@ class NormalUser < ApplicationRecord
 
   has_many :credits, dependent: :destroy
 
-  def add_credits(valor = 0)
+  def add_credits(valor.to_i = 0)
     if valor <= 0
       errors[:base] << "Insira um valor válido."
       return false
     end
-    credits = self.credits.build(value: valor)
+    credits = self.credits.build(value: valor.to_s)
     if credits.save
       return credits
     else
