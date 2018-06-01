0b9247416093da79ad222c490e59a974a8c7fe07
Matheus Perrut <matheus.perrut2302@gmail.com>
2018-06-01 11:35:23 -0300

    Fixing migrations, duplicate latitude adn longitude

--- db/migrate/20180601031120_add_latitude_longitude_estacionamento.rb
+++
@@ -1,7 +0,0 @@
-class AddLatitudeLongitudeEstacionamento < ActiveRecord::Migration[5.1]
-  def change
-    add_column :estacionamentos, :latitude, :string
-    add_column :estacionamentos, :longitude, :string
-    
-  end
-end
