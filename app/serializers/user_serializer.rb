db5aa57913bba1f2319f31e336bedb3046704ef9
Matheus Perrut <matheus.perrut2302@gmail.com>
2018-06-08 13:26:17 -0300

    Adicionando operador estac. ao serializer de user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :lastname, :birth, :tel, :authentication_token, :email
 
   has_one :store_owner
-
   has_one :normal_user
   has_one :dono_estacionamento
+  has_one :operador_estacionamento
 end
