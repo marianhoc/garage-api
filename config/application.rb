6241a81f469cd9e3b2a4187372435e824bbd83a3
Matheus Perrut <matheus.perrut2302@gmail.com>
2018-06-14 13:44:02 -0300

    Correção do fuso horário :(

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module ApiPareMais
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Configuração do fuso horário Brasileiro
+    config.time_zone = 'Brasilia'
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
